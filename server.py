9f10a7bfc450da793eb214d444e3d08e4a53b06a
SwalveSam <samantha.swalve@gmail.com>
2021-07-14 01:22:09 +0000

    first Commit

---
+++ server.py
@@ -0,0 +1,20 @@
+# server for personal website
+
+# to render flask server 
+from flask import (Flask, render_template, request, flash, session, jsonify,
+                   redirect)
+
+
+app = Flask(__name__)
+app.secret_key = "secret-key"
+
+
+@app.route('/')
+def homepage():
+
+    return render_template('homepage.html')
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(use_reloader=True, use_debugger=True)
